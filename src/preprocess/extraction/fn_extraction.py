f855969b8708a2f57d18d308c8b5d5f41f9b8b53
Marcos Mota <marcos.mota287@gmail.com>
2021-09-09 13:28:56 -0300

    adicionando teste e logs nas funcoes lambdas

--- src/preprocess/extraction/fn_extraction.py
+++ src/preprocess/extraction/fn_extraction.py
@@ -1,27 +1,50 @@
 import json
 import os
-
 import boto3
 import requests
+import logging
 
-REGION_NAME = os.environ['AWS_REGION']
-STREAM_NAME = os.environ['PROJECT_STEAM']
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def environIsValid(aws_region: str, kinesis_steam: str) -> bool:
+    if aws_region is None:
+        logger.error("Variável de Ambiente AWS_REGION está vazia.")
+        return False
+
+    if kinesis_steam is None:
+        logger.error("Variável de Ambiente PROJECT_STEAM está vazia.")
+        return False
+
+    return True
 
 
 def handler(event, lambda_context):
-    KINESIS_CLIENT = boto3.client('kinesis', region_name=REGION_NAME)
-    # Realizando o get na API para a ingestao dos dados
-    response = requests.get("https://api.punkapi.com/v2/beers/random")
-    data = response.json()[0]
+    aws_region = os.environ.get('AWS_REGION', None)
+    kinesis_steam = os.environ.get('PROJECT_STEAM', None)
 
-    # Enviando os dados para o Kinesis
-    put_response = KINESIS_CLIENT.put_record(
-        StreamName=STREAM_NAME,
-        Data=json.dumps(data),
-        PartitionKey="partition_key"
-    )
+    if environIsValid(aws_region=aws_region, kinesis_steam=kinesis_steam) is False:
+        raise Exception("Variáveis AWS_REGION e PROJECT_STEAM não estão definidas")
 
-    return data
+    try:
+        kinesis_client = boto3.client('kinesis', region_name=aws_region)
+        response = requests.get("https://api.punkapi.com/v2/beers/random")
+
+        if response.status_code > 200:
+            raise Exception("Problemas encontrado ao obter dados da API")
+
+        response = kinesis_client.put_record(
+            StreamName=kinesis_steam,
+            Data=json.dumps(response.json()[0]),
+            PartitionKey="partition_key"
+        )
+
+        logger.info(f"Quantidade de eventos - {len(response.json)}")
+
+        return response
+    except Exception as ex:
+        logger.error("Problemas encontrados ao executar a função de extração", ex)
 
 
 if __name__ == '__main__':
--- test/test_dataingestion.py
+++ test/test_dataingestion.py
@@ -1,50 +1,24 @@
+import os
 import unittest
+from src.preprocess.extraction import fn_extraction
 
-import boto3
-from moto import mock_s3, mock_kinesis
 
-S3_BUCKET_NAME = 'amaksimov-s3-bucket'
-DEFAULT_REGION = 'us-east-1'
-from moto.core import ACCOUNT_ID
-
-S3_TEST_FILE_KEY = 'prices/new_prices.json'
-S3_TEST_FILE_CONTENT = [
-    {"product": "Apple", "price": 15},
-    {"product": "Orange", "price": 25}
-]
-@mock_kinesis()
-@mock_s3
 class DataIngestionCase(unittest.TestCase):
-    def test_getbeers_with_success(self):
-        self.assertEqual(True, True)
 
-    def test_getbeers_with_fail(self):
-        self.assertEqual(True, False)
+    def test_environment_region_required(self):
+        os.environ['PROJECT_STEAM'] = "teste"
 
-    def test_clear_dataset_with_success(self):
-        self.assertEqual(True, False)
+        with self.assertRaises(Exception) as context:
+            func = fn_extraction.handler('', '')
 
-    def test_clear_dataset_with_fail(self):
-        self.assertEqual(True, False)
+    def test_environment_steam_required(self):
+        os.environ['AWS_REGION'] = 'teste'
 
-    @mock_kinesis
-    def test_describe_stream_summary(self):
-        conn = boto3.client("kinesis", region_name="us-west-2")
-        stream_name = "my_stream_summary"
-        shard_count = 5
-        conn.create_stream(StreamName=stream_name, ShardCount=shard_count)
+        with self.assertRaises(Exception) as context:
+            func = fn_extraction.handler('', '')
 
-        resp = conn.describe_stream_summary(StreamName=stream_name)
-        stream = resp["StreamDescriptionSummary"]
-        print()
-        print(stream_name)
-        self.assertEqual(stream["StreamName"], stream_name)
-        stream["StreamName"].should.equal(stream_name)
-        stream["OpenShardCount"].should.equal(shard_count)
-        stream["StreamARN"].should.equal(
-            "arn:aws:kinesis:us-west-2:{}:{}".format(ACCOUNT_ID, stream_name)
-        )
-        stream["StreamStatus"].should.equal("ACTIVE")
+        # self.assertTrue('Variáveis AWS_REGION e PROJECT_STEAM não estão definidas' in context.exception)
+
 
 if __name__ == '__main__':
     unittest.main()
